a2f3054072232498c1682887de61b1787b74ac6b
n3f1l1m <kirakosyangev@yahoo.com>
2021-08-29 00:47:26 +0400

    little bugfix little bugfix

---
+++ src/Component/Main/About/About.jsx
@@ -0,0 +1,13 @@
+import "./About.scss";
+function About() {
+  return (
+    <div className="About">
+      <h2>About</h2>
+      <div className="postDesc">
+        This project created by React js for education.
+      </div>
+    </div>
+  );
+}
+
+export default About;
